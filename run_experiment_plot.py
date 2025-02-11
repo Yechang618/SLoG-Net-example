f49cdba25140860831a4bd7dc2de4fc0985b7256
Yechang618 <yechang618@gmail.com>
2025-02-11 18:07:44 -0500

    Update run_experiment_plot.py

--- run_experiment_plot.py
+++ run_experiment_plot.py
@@ -90,76 +90,11 @@ def run_experiment(config_file):
 
 
     # Plot results
-    fig, ax = plt.subplots(nrows=2, ncols=1, constrained_layout=True,figsize=(10,5))
-    ax[0].plot(noiseLvs, acc_mean_admm, color='#CC4F1B', linewidth=2, label='ADMM')
-    ax[0].fill_between(noiseLvs, acc_mean_admm - acc_std_admm, acc_mean_admm + acc_std_admm, alpha=0.2, color='#FF9848')
-    ax[0].plot(noiseLvs, acc_mean, color='#1B2ACC', linewidth=2, label='SLoG-Net')
-    ax[0].fill_between(noiseLvs, acc_mean - acc_std, acc_mean + acc_std, alpha=0.2, color='#089FFF')
-
-    ax[0].set_xlabel('Noise Level')
-    ax[0].set_ylabel('Accuracy')
-    ax[0].set_ylim([0, 1])
-    ax[0].legend()
-    ax[0].grid()
-
-    ax[1].plot(noiseLvs, rex_mean_admm, color='#CC4F1B', linewidth=2, label='ADMM')
-    ax[1].fill_between(noiseLvs, rex_mean_admm - rex_std_admm, rex_mean_admm + rex_std_admm, alpha=0.2, color='#FF9848')
-    ax[1].plot(noiseLvs, rex_mean, color='#1B2ACC', linewidth=2, label='SLoG-Net')
-    ax[1].fill_between(noiseLvs, rex_mean - rex_std, rex_mean + rex_std, alpha=0.2, color='#089FFF')
-
-    ax[1].set_xlabel('Noise Level')
-    ax[1].set_ylabel('RE_x')
-    ax[1].set_ylim([0, 1])
-    ax[1].legend()
-    ax[1].grid()    
-
-    # Save figure
-    output_dir = "plots"
-    os.makedirs(output_dir, exist_ok=True)
-    save_path = os.path.join(output_dir, "accuracy_plot.png")
-    # save_path = "accuracy_plot.png"
-    fig.savefig(save_path, format="pdf",transparent=True)
-    print(f"Figure saved at {save_path}")
-
-    plt.show()
-
-    ################################################################################
-    ######################### Saved figure  ####################################
-    ################################################################################
     legend_fontsize = 15
     label_fontsize = 20
     line_wid = 4
 
-    [N_noiseLvs,N_model,N_realiz]  = result_exp_acc_slog.shape
-    acc_arg = result_exp_acc_slog.reshape((N_noiseLvs,N_model*N_realiz))
-    acc_arg_admm = result_exp_acc_admm.reshape((N_noiseLvs,N_model*N_realiz))
-    acc_mean = np.mean(acc_arg,axis = 1)
-    acc_std = np.std(acc_arg,axis = 1)
-    acc_mean_admm = np.mean(acc_arg_admm,axis = 1)
-    acc_std_admm = np.std(acc_arg_admm,axis = 1)
-
-    rex_arg = result_exp_rex_slog.reshape((N_noiseLvs,N_model*N_realiz))
-    reg_arg = result_exp_reg_slog.reshape((N_noiseLvs,N_model*N_realiz))
-    elps_arg = result_elapse_slog.reshape((N_noiseLvs,N_model*N_realiz))
-    rex_arg_admm = result_exp_rex_admm.reshape((N_noiseLvs,N_model*N_realiz))
-    reg_arg_admm = result_exp_reg_admm.reshape((N_noiseLvs,N_model*N_realiz))
-    elps_arg_admm = result_elapse_admm.reshape((N_noiseLvs,N_model*N_realiz))
-
-    rex_mean = np.mean(rex_arg,axis = 1)
-    rex_std = np.std(rex_arg,axis = 1)
-    reg_mean = np.mean(reg_arg,axis = 1)
-    reg_std = np.std(reg_arg,axis = 1)
-    elps_mean = np.mean(elps_arg,axis = 1)
-    elps_std = np.std(elps_arg,axis = 1)
-
-    rex_mean_admm = np.mean(rex_arg_admm,axis = 1)
-    rex_std_admm = np.std(rex_arg_admm,axis = 1)
-    reg_mean_admm = np.mean(reg_arg_admm,axis = 1)
-    reg_std_admm = np.std(reg_arg_admm,axis = 1)
-    elps_mean_admm = np.mean(elps_arg_admm,axis = 1)
-    elps_std_admm = np.std(elps_arg_admm,axis = 1)
-
-    fig_save_2, axes = plt.subplots(nrows=2, ncols=1, constrained_layout=True,figsize=(10,5))
+    fig, axes = plt.subplots(nrows=2, ncols=1, constrained_layout=True,figsize=(10,5))
 
     axes[0].plot(noiseLvs,rex_mean_admm, color='#CC4F1B',linewidth=line_wid, label='ADMM')
     axes[0].fill_between(noiseLvs,rex_mean_admm - rex_std_admm, rex_mean_admm + rex_std_admm,alpha= 0.2, edgecolor='#CC4F1B', facecolor='#FF9848',
@@ -185,6 +120,15 @@ def run_experiment(config_file):
     axes[1].set_ylim([0.0,1.0])
     axes[1].legend(loc = 'best',fontsize = 15)
     axes[1].grid()
+
+    # Save figure
+    output_dir = "plots"
+    os.makedirs(output_dir, exist_ok=True)
+    save_path = os.path.join(output_dir, "accuracy_plot.pdf")
+    fig.savefig(save_path, format="pdf", transparent=True)
+    print(f"Figure saved at {save_path}")
+
+    plt.show()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run SLoG-Net Experiment")
